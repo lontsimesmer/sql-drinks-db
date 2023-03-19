a41087e151cf3b2e767302d7aebd319581d385d4
LONTSI TCHOUPOU Nathanaël Mesmer <tchopounathanael@gmail.com>
2023-03-20 00:03:44 +0100

    modified index page

--- database/index.js
+++ database/index.js
@@ -1,9 +1,14 @@
-const {Sequelize} = require("sequelize");
+const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("rebase_drink_db", "mesmer", "nathanael.1990", {
-  host: "db4free.net",
-  dialect: "mysql",
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
 module.exports = sequelize;
 
